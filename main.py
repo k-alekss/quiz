b7ceb349341340cef2aa4e9c04e88898bcba1f55
UserITB <sasha2001200969@gmail.com>
2025-02-13 20:55:02 +0400

    add timer

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import time
+
 questions = {
     'Сколько планет в Солнечной системе?': '8',
     'Столица Франции?': 'Париж'
@@ -5,7 +7,13 @@ questions = {
 
 score = 0
 for q, a in questions.items():
+    start_time = time.time()
     user_answer = input(q + ' ')
+    stop_time = time.time()
+    if round(stop_time - start_time, 1) > 5:
+        print('Вы не уложились в 5 секунд.')
+        user_answer = 'wrong'
+    print(f'Время ответа: {round(stop_time - start_time, 1)} секунд.')
     if user_answer.lower() == a.lower():
         print('Правильно!')
         score += 1
